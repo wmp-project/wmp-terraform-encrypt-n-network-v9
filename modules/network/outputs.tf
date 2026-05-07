95ac0fcd36339c5616420c027ea2ba24e7a82143
Raghu K <raghuk.vit@gmail.com>
2026-05-07 07:49:51 +0530

    WMP scripts

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -2,11 +2,11 @@ output "vpc_id" {
   value = aws_vpc.main
 }
 
-output "db_subnet_ids" {
-  value = [ for i,j in local.subnets_merged: j.id if j.group == "db" ]
-}
-
-output "app_subnet_ids" {
-  value = [ for i,j in local.subnets_merged: j.id if j.group == "app" ]
-}
-
+# output "db_subnet_ids" {
+#   value = [ for i,j in local.subnets_merged: j.id if j.group == "db" ]
+# }
+#
+# output "app_subnet_ids" {
+#   value = [ for i,j in local.subnets_merged: j.id if j.group == "app" ]
+# }
+#
