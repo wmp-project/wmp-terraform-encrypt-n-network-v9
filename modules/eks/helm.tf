ff8c97a07fd16811bb3b66852c873beef155e7c2
Raghu K <raghuk.vit@gmail.com>
2026-05-11 08:17:34 +0530

    terraform: update 1 file(s) (helm.tf)

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -44,20 +44,20 @@ resource "helm_release" "argocd" {
   ]
 }
 
-resource "helm_release" "kube-stack" {
-
-  depends_on = [null_resource.kube-config, helm_release.traefik]
-
-  name       = "kubestack"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  set = [
-    {
-      name  = "prometheus.service.type"
-      value = "LoadBalancer"
-    }
-  ]
-}
+# resource "helm_release" "kube-stack" {
+#
+#   depends_on = [null_resource.kube-config, helm_release.traefik]
+#
+#   name       = "kubestack"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   chart      = "kube-prometheus-stack"
+#   set = [
+#     {
+#       name  = "prometheus.service.type"
+#       value = "LoadBalancer"
+#     }
+#   ]
+# }
 
 resource "helm_release" "file-beat" {
 
