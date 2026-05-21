736c0deca629df2c022c9d4bc391611ce3673d9a
murali7032 <muralikrishnareddy456@gmail.com>
2026-05-21 17:15:44 +0530

    class change

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -98,7 +98,7 @@ resource "aws_eks_node_group" "main" {
   node_group_name = "${var.env}-ng"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnets
-  instance_types  = ["t3.small"]
+  instance_types  = ["m7i-flex.large"]
   capacity_type   = "ON_DEMAND"
   launch_template {
     version = "$Latest"
